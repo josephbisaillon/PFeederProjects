2dd4954cb3b0915ce6a2ff7cfb407d2ec135f27a
Joseph Bisaillon <bisaillonjd@gmail.com>
2015-09-24 20:16:01 -0400

    initial commit

---
+++ PFeederProjects/ElephantServer/Tools/EConsole.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ElephantServer.UI;
+
+namespace ElephantServer.Tools
+{
+    public static class EConsole
+    {
+        /// <summary>
+        /// Append text to the Elephant Console
+        /// </summary>
+        /// <param name="line">Output string you want on the console</param>
+        public static void WriteLine(string line)
+        {
+            // Append the line to the Elephant Console box
+            UIControls.uElephantConsole.Text += line;
+        }
+    }
+}
---
+++ PFeederProjects/ElephantServer/UI/UIControls.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace ElephantServer.UI
+{
+    public class UIControls : INotifyPropertyChanged
+    {
+        private string _consoleline;
+        //private string _organization;
+
+        public string ConsoleLine
+        {
+            get { return _consoleline; }
+            set
+            {
+                _consoleline = value;
+                RaisePropertyChanged("Name");
+            }
+        }
+
+        //public string Organization
+        //{
+        //    get { return _organization; }
+        //    set
+        //    {
+        //        _organization = value;
+        //        RaisePropertyChanged("Organization");
+        //    }
+        //}
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void RaisePropertyChanged(string name)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(name));
+            }
+        } 
+    }
+}
